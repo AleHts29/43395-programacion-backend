f20b44c3247e089c441308af28528d224853b1d6
Alejandro Huertas <ahuertas@fi.uba.ar>
2023-06-26 18:08:21 -0300

    first commit

---
+++ Clase_00/index.js
@@ -0,0 +1,26 @@
+let nombre = "Ana"
+const numero = 12;
+let numero2 = '12';
+
+// numero = true
+
+const obj = {
+    nombre: "Tomas",
+    apellido: "Torres"
+}
+console.log(obj);
+
+const array = [1, 2, "Ana", true]
+
+// obj.nombre = "Diana" // esto esta OK
+// console.log(obj);
+
+// esto no se puede hacer
+obj = {
+    nombre: "Diana",
+    apellido: "Torres"
+}
+console.log(obj);
+
+
+// console.log(typeof (array));
