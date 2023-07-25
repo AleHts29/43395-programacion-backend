6073144189bca4a262080054602b94c9ca0afae9
Alejandro Huertas <ahuertas@fi.uba.ar>
2023-07-24 21:40:01 -0300

    clase09

---
+++ Clase_09/src/index.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import handlebars from 'express-handlebars'
+import __dirname from './utils.js';
+// import usersRoutes from './routes/users.routes.js'
+
+const app = express();
+const PORT = 9090;
+
+//Preparar la configuracion del servidor para recibir objetos JSON.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// Para trabajar con archivos estaticos
+// app.use('/static', express.static('./src/public'))
+app.use(express.static(__dirname + '/public'))
+
+
+// Configuracion de .hbs
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + '/views/');
+app.set('view engine', 'handlebars');
+
+
+
+app.get('/', (req, res) => {
+    // usuario de prueba
+    let testUser = {
+        name: 'Santiago',
+        last_name: 'Kosacoff',
+        edad: 26
+    }
+    res.render('hello', testUser);
+})
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`server run on port: ${PORT}`);
+})
---
+++ Clase_09/src/utils.js
@@ -0,0 +1,10 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import multer from 'multer';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename)
+
+export default __dirname;
+
+
