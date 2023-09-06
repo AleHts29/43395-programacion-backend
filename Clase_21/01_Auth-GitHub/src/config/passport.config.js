8ac5eaa46fe5881133772e665c6c9da4aad20317
Alejandro Huertas <ahuertas@fi.uba.ar>
2023-09-06 20:42:36 -0300

    clase21

---
+++ Clase_21/01_Auth-GitHub/src/config/passport.config.js
@@ -0,0 +1,97 @@
+import passport from 'passport';
+import passportLocal from 'passport-local';
+import GitHubStrategy from 'passport-github2';
+import userModel from '../models/user.model.js';
+import { createHash, isValidPassword } from '../utils.js';
+
+//Declaramos nuestra estrategia:
+const localStrategy = passportLocal.Strategy;
+const initializePassport = () => {
+    /**
+      *  Inicializando la estrategia para github.
+      *  Done será nuestro callback
+     */
+    /*=============================================
+    =                GitHubStrategy               =
+   =============================================*/
+    // TODO: Estrategia de Login con GitHub
+
+
+
+
+
+
+
+
+
+    /*=============================================
+    =                localStrategy                =
+    =============================================*/
+    //Estrategia de registro de usuario
+    passport.use('register', new localStrategy(
+        { passReqToCallback: true, usernameField: 'email' }, async (req, username, password, done) => {
+            const { first_name, last_name, email, age } = req.body;
+            try {
+                const exists = await userModel.findOne({ email });
+                if (exists) {
+                    console.log("El usuario ya existe.");
+                    return done(null, false);
+                }
+                const user = {
+                    first_name,
+                    last_name,
+                    email,
+                    age,
+                    password: createHash(password),
+                    loggedBy: "App"
+                };
+                const result = await userModel.create(user);
+                //Todo sale OK
+                return done(null, result);
+            } catch (error) {
+                return done("Error registrando el usuario: " + error);
+            }
+        }
+    ));
+
+    //Estrategia de Login de la app:
+    passport.use('login', new localStrategy(
+        { passReqToCallback: true, usernameField: 'email' }, async (req, username, password, done) => {
+            try {
+                const user = await userModel.findOne({ email: username });
+                console.log("Usuario encontrado para login:");
+                console.log(user);
+                if (!user) {
+                    console.warn("User doesn't exists with username: " + username);
+                    return done(null, false);
+                }
+                if (!isValidPassword(user, password)) {
+                    console.warn("Invalid credentials for user: " + username);
+                    return done(null, false);
+                }
+                return done(null, user);
+            } catch (error) {
+                return done(error);
+            }
+        })
+    );
+
+
+    /*=============================================
+    = Funciones de Serializacion y Desserializacion =
+    =============================================*/
+    passport.serializeUser((user, done) => {
+        done(null, user._id);
+    });
+
+    passport.deserializeUser(async (id, done) => {
+        try {
+            let user = await userModel.findById(id);
+            done(null, user);
+        } catch (error) {
+            console.error("Error deserializando el usuario: " + error);
+        }
+    });
+};
+
+export default initializePassport;
---
+++ Clase_21/01_Auth-GitHub/src/routes/github-login.views.router.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const router = Router();
+
+// TODO: Routers de vistas
+
+export default router;
