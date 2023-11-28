c5029af3d5f39c602e9451d12c525ca9e49df52e
Alejandro Huertas <ahuertas@fi.uba.ar>
2023-11-28 11:31:23 -0300

    clase42

---
+++ Clase_42/primer-proyecto-nest/src/user/user.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpException, HttpStatus } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) { }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    if (!createUserDto.first_name || !createUserDto.first_name) throw new HttpException("Datos incompletos ", HttpStatus.BAD_REQUEST)
+    return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    const users = this.userService.findAll();
+    return { status: "Success", payload: users }
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(+id);
+  }
+}
